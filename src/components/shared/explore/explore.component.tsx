28d9a90407bd5b9cce93ce5b3e1753596dc8e46d
Atul Vinod <atul.v@keka.com>
2022-04-27 16:26:22 +0530

    added explore button shadow

--- src/components/shared/explore/explore.component.tsx
+++ src/components/shared/explore/explore.component.tsx
@@ -2,8 +2,11 @@ export const Explore = () => {
   return (
     <div className="explore">
       <div className="explore-button">
-        <div>
+        <div className="foreground">
           <h4 className="text-color__black text-font__400">Explore</h4>
+        </div>
+        <div className="background">
+          
         </div>
       </div>
     </div>
